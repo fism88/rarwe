d77e00815f7fda808e1881321a3069db1538daaf
Faraaz Ismail <fism88@gmail.com>
2018-02-06 22:13:49 -0600

    Nested band and songs routes as children of bands route

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,22 +1,64 @@
-import EmberObject from '@ember/object';
+import EmberObject, { computed } from '@ember/object';
 import Route from '@ember/routing/route';
 
 var Band = EmberObject.extend({
-  name: ''
+  name: '',
+  language: '',
+
+  slug: computed('name', function() {
+    return this.get('name').dasherize();
+  }),
+
+  site: computed('slug', 'language', function () {
+    return 'http://bands.com/' + this.get('slug') + '.' + this.get('language');
+  })
+});
+
+var Song = EmberObject.extend({
+  title: '',
+  band: '',
+  rating: 0
 });
 
 export default Route.extend({
   model: function () {
+    var blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
+
+    var yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    var pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    var daughter = Song.create({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5
+    });
+
     var ledZeppelin = Band.create({
-      name: 'Led Zeppelin'
+      name: 'Led Zeppelin',
+      songs: [blackDog]
     });
 
     var pearlJam = Band.create({
-      name: 'Pearl Jam'
+      name: 'Pearl Jam',
+      songs: [yellowLedbetter, daughter]
     });
 
     var fooFighters = Band.create({
-      name: 'Foo Fighters'
+      name: 'Foo Fighters',
+      songs: [pretender]
     });
 
     return [ledZeppelin, pearlJam, fooFighters];
