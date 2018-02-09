576be6bcd8d30b39c0a656126f461bfb5470f397
Faraaz Ismail <fism88@gmail.com>
2018-02-09 13:46:52 -0600

    Acceptance test for creating a song in two steps; fix element class names to match test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -96,3 +96,64 @@ test('Create a new band', function (assert) {
     assert.equal(find('.nav a.active:contains("Songs")').length, 1, 'The Songs tab is active');
   });
 });
+
+test('Create a new song in two steps', function (assert) {
+  server = new Pretender(function () {
+    this.get('/bands', function () {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          }
+        ]
+      };
+
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+
+    this.get('/bands/1', function () {
+      var response = {
+        data: {
+          id: 1,
+          type: 'bands',
+          attributes: {
+            name: 'Radiohead'
+          }
+        }
+      };
+
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+
+    this.post('/songs', function () {
+      var response = {
+        data: {
+          id: 1,
+          type: 'songs',
+          attributes: {
+            name: 'Killer Cars',
+          }
+        }
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+
+    this.get('/bands/1/songs', () => {
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify({data: []})];
+    });
+  });
+
+  visit('/');
+  click('.band-link:contains("Radiohead")');
+  click('a:contains("create one")');
+  fillIn('.new-song', 'Killer Cars');
+  triggerEvent('.new-song-form', 'submit');
+
+  andThen(function () {
+    assert.equal(find('.songs .song:contains("Killer Cars")').length, 1, 'Creates the song and displays it in the list');
+  });
+});
