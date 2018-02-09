f89e7d4ea41757487bee4dcc6529a537934d528e
Faraaz Ismail <fism88@gmail.com>
2018-02-09 13:18:25 -0600

    Creating a band acceptance test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -10,7 +10,7 @@ moduleForAcceptance('Acceptance | bands', {
 
 var server;
 
-test('List bands', function(assert) {
+test('List bands', function (assert) {
   server = new Pretender(function () {
     this.get('/bands', function () {
       var response = {
@@ -42,5 +42,57 @@ test('List bands', function(assert) {
     assert.equal(find('.band-link').length, 2, 'All band links are rendered');
     assert.equal(find('.band-link:contains("Radiohead")').length, 1, 'First band link contains the band name');
     assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, 'The other band link contains the band name');
+  });
+});
+
+test('Create a new band', function (assert) {
+  server = new Pretender(function () {
+    this.get('/bands', function () {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          }
+        ]
+      };
+
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+
+    this.post('/bands', function () {
+      var response = {
+        data: {
+          id: 2,
+          type: 'bands',
+          attributes: {
+            name: 'Long Distance Calling'
+          }
+        }
+      };
+
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+
+    this.get('/bands/2/songs', function () {
+      var response = {
+        data: []
+      };
+
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+  });
+
+  visit('/bands');
+  fillIn('.new-band', 'Long Distance Calling');
+  click('.new-band-button');
+
+  andThen(function () {
+    assert.equal(find('.band-link').length, 2, 'All band links are rendered');
+    assert.equal(find('.band-link:last').text().trim(), 'Long Distance Calling', 'Created band appears at the end of the list');
+    assert.equal(find('.nav a.active:contains("Songs")').length, 1, 'The Songs tab is active');
   });
 });
