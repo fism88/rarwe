17b7cce21b2892ba657966f65b5a869250383ea2
Faraaz Ismail <fism88@gmail.com>
2018-02-11 14:01:52 -0600

    Allow sorting songs by rating/title; allow searching for songs; acceptance tests for searching/sorting songs

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,11 +1,9 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import { sort } from '@ember/object/computed';
 import { isEmpty } from '@ember/utils';
 
 export default Controller.extend({
-  songCreationStarted: false,
-  title: '',
-
   actions: {
     enableSongCreation () {
       this.set('songCreationStarted', true);
@@ -24,11 +22,46 @@ export default Controller.extend({
     }
   },
 
+  canCreateSong: computed('songCreationStarted', 'model.songs.[]', function () {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
+
   isAddButtonDisabled: computed('title', function () {
     return isEmpty(this.get('title'));
   }),
 
-  canCreateSong: computed('songCreationStarted', 'model.songs.[]', function () {
-    return this.get('songCreationStarted') || this.get('model.songs.length');
+  matchingSongs: computed('model.songs.@ech.title', 'searchTerm', function () {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+
+    return this.get('model.songs').filter(function (song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
   }),
+
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's',
+  },
+
+  searchTerm: '',
+
+  songCreationStarted: false,
+
+  sortBy: 'ratingDesc',
+
+  sortProperties: computed('sortBy', function () {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc'
+    };
+
+    return options[this.get('sortBy')].split(',');
+  }),
+
+  sortedSongs: sort('matchingSongs', 'sortProperties'),
+
+  title: '',
+
 });
